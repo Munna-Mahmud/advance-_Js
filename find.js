d59c24ae0afb99810645a4bd30c97416f24898b3
Munna-Mahmud <learnermunna@gmail.com>
2021-09-16 15:45:14 +0600

    practise find and findIndex

---
+++ find.js
@@ -0,0 +1,5 @@
+const numbers =[3, 34, 5, 4, 23, 23]
+const results = numbers.find(function(currenvalue){
+    return currenvalue > 20;
+})
+console.log(results)
---
+++ findIndex.js
@@ -0,0 +1,9 @@
+const numbers =[3, 34, 5, 4, 23, 23]
+const results = numbers.findIndex(function(currenvalue){
+    return currenvalue > 20;
+})
+console.log(results)
+
+//find gives us the element
+//and
+//findIndex gives us the index  number here  i can see 34 index number 1 that's why i get 1 output 
